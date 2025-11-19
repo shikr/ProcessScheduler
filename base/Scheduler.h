a87ece76829f822dae562c42c2ee000538a891ed
shikr <cristobal.rod0907@gmail.com>
2025-11-19 00:01:13 -0600

    Overload ostream operator

--- base/Scheduler.h
+++ base/Scheduler.h
@@ -4,23 +4,26 @@
 
 #ifndef PROCESSSCHEDULER_SCHEDULER_H
 #define PROCESSSCHEDULER_SCHEDULER_H
+#include <ostream>
 #include <queue>
-#include "Process.h"
-#include "../memory/BaseMemory.h"
 
+#include "../memory/BaseMemory.h"
+#include "Process.h"
 
 class Scheduler {
     std::queue<Process> processes;
     std::queue<Process> blockedProcesses;
     int maxMemory;
     int maxQuantum;
-    BaseMemory *memoryManager;
+    BaseMemory* memoryManager;
     void addProcess(const Process& process);
 
-public:
-    Scheduler(const int maxMemory, const int maxQuantum, BaseMemory* memory) : maxMemory(maxMemory), maxQuantum(maxQuantum), memoryManager(memory) {};
+   public:
+    Scheduler(const int maxMemory, const int maxQuantum, BaseMemory* memory)
+        : maxMemory(maxMemory), maxQuantum(maxQuantum), memoryManager(memory) {};
     [[noreturn]] void schedule();
+
+    friend std::ostream& operator<<(std::ostream&, const Scheduler&);
 };
 
-
-#endif //PROCESSSCHEDULER_SCHEDULER_H
+#endif  // PROCESSSCHEDULER_SCHEDULER_H
